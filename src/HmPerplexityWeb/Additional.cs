7ec6548ffb4fb282b8c51d745170e74666408fcd
komiyamma <komiyama0629@gmail.com>
2025-07-08 15:05:13 +0900

    AIの返答を「選択中」の状態のまま、新たな質問内容を秀丸から送信した場合でも、うまく動作するように、「CTRL」＋「J」を送信するようにした。

---
+++ src/HmPerplexityWeb/Additional.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HmPerplexityWeb;
+
+public partial class HmPerplexityWeb
+{
+    private const byte VK_ESC = 0x1B; // ESCキーの仮想キーコード
+
+    public void SendCtrlJKeySync()
+    {
+        SendCtrlJKey();
+    }
+
+    private async void SendCtrlJKey()
+    {
+        // Ctrl キーを解放
+        keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+        // Alt キーを解放
+        keybd_event(VK_ALT, 0, KEYEVENTF_KEYUP, 0);
+
+        // shiftキーを解放
+        keybd_event(VK_SHIFT, 0, KEYEVENTF_KEYUP, 0);
+
+
+        await Task.Delay(100);
+
+        // CTRLキーを押す
+        keybd_event(VK_CONTROL, 0, 0, 0);
+
+        // Jキーを押す
+        keybd_event((byte)'J', 0, 0, 0);
+
+        await Task.Delay(200);
+
+        // Jキーを離す
+        keybd_event((byte)'J', 0, KEYEVENTF_KEYUP, 0);
+        // CTRLキーを離す
+        keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+
+    }
+
+
+}
